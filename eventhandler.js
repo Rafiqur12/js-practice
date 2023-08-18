95716f3d34668f23c0b9b1ff6018fe1405f3856d
Ripon <ripon18922@gmail.com>
2023-08-19 03:18:40 +0600

    event Handler

---
+++ eventhandler.js
@@ -0,0 +1,13 @@
+// function myMessage1(){
+//     alert("I am button One");
+// }
+// function myMessage2(){
+//     alert("I am button two");
+// }
+var myVar= document.querySelector("#paraId");
+function myMessage1(){
+    myVar.innerHTML="You have clicked on button1";
+}
+function myMessage2(){
+    myVar.innerHTML="You have clicked on button2";
+}
