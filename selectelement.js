738b9923993490552307674759bf3ec0d7b6e9fc
Ripon <ripon18922@gmail.com>
2023-08-19 02:45:04 +0600

    select element in html

---
+++ selectelement.js
@@ -0,0 +1,4 @@
+// document.getElementById("heading1").innerHTML="Hello";
+var myheading=document.getElementById("heading1");
+myheading.innerHTML="Hello"
+document.getElementById("para").innerHTML="Bye";
