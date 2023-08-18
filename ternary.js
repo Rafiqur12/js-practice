08d4483ff52324d92866370859a78242e7bf3e31
Ripon <ripon18922@gmail.com>
2023-08-18 11:15:23 +0600

    Ternary is added

---
+++ ternary.js
@@ -0,0 +1,14 @@
+var number= Number(prompt("Enter a number "));
+// if(number>0){
+//     console.log("Positive");
+// }
+// else if (number<0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("Zero");
+// }
+// var result= number>0 ? "positive":"negative";
+// console.log(result);
+var result=number>0 ? "Positive" : number<0 ? "Negative":"Zero";
+console.log(result);33
